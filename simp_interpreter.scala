aa6da94469b58ac377efc6a019940ae24b7e4ebf
Hari Limaye <hari.limaye@kcl.ac.uk>
2021-02-01 18:30:54 +0100

    Implement run method - evaluates integer expressions

--- simp_interpreter.scala
+++ simp_interpreter.scala
@@ -8,17 +8,29 @@ trait Result
 trait Program extends Instruction with Result   
 
 // Operators
-abstract class Operator extends Instruction
-case object Plus extends Operator
-case object Minus extends Operator
-case object Times extends Operator
-case object Divide extends Operator
+trait Operator extends Instruction {
+    def evaluate(n1: Int, n2: Int) : Int
+}
+case object Plus extends Operator { 
+    override def evaluate(n1: Int, n2: Int) : Int = n1 + n2
+}
+case object Minus extends Operator { 
+    override def evaluate(n1: Int, n2: Int) : Int = n1 - n2
+}
+case object Times extends Operator { 
+    override def evaluate(n1: Int, n2: Int) : Int = n1 * n2
+}
+// Division by 0 defined as 0
+case object Divide extends Operator { 
+    override def evaluate(n1: Int, n2: Int) : Int = 
+        if (n2 != 0) n1 / n2 else 0
+}
 
 // Locations or Variables
 case class Location(id: String) extends Result
 
 // Integer Expressions
-abstract class Exp extends Program
+trait Exp extends Program
 case class ValueAt(loc: Location) extends Exp
 case class Num(n: Int) extends Exp
 case class BinOp(e1: Exp, op: Operator, e2: Exp) extends Exp
@@ -29,5 +41,23 @@ type Results = List[Result]
 type Memory = Map[Location, Int]
 type Configuration = (Control, Results, Memory)
 
+// Interpreter - recursively computes transformations from configurations
+// Currently has "unnecessary steps" from literally following transition rules
+def run(conf: Configuration) : Configuration = conf match {
+    case (Num(x)::c, r, m) => 
+        run((c, Num(x)::r, m))
+    case (BinOp(e1, op, e2)::c, r, m) => 
+        run((e1::e2::op::c, r, m))
+    case ((op:Operator)::c, Num(n2)::Num(n1)::r, m) =>
+        val n = op.evaluate(n1, n2)
+        run((c, Num(n)::r, m))    
+    case (ValueAt(loc)::c, r, m) =>    
+        val n = m(loc)
+        run((c, Num(n)::r, m))
+    case (Nil, r, m) =>
+        conf    
+}
 
-
+val e = BinOp(Num(10), Times, Num(10))
+val init : Configuration = (e::Nil, Nil, Map())
+run(init)
